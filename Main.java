4774432b8af661dea46a2f2288b4b2b96129a644
Utkarsh <164793896+utkarsh-64@users.noreply.github.com>
2025-02-03 21:44:03 +0530

    Adding ArrayInput class
    added the ArrayInput class and its methods to handle user input

--- Main.java
+++ Main.java
@@ -2,5 +2,10 @@
 import java.util.*;
 
 class Main {
-    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in); 
+        ArrayInput Ai = new ArrayInput(scan); // Create ArrayInput object
+
+        int[] userArray = Ai.userInput(); // Get array input from user
+
 }
