93f5bceb3a3d51a338c9b9a3c8f709cf61d3c0f4
Utkarsh <164793896+utkarsh-64@users.noreply.github.com>
2025-02-03 21:45:40 +0530

    Adding the ArrayFunction class and calling functions
    added the logic for odd/even separation, minimum consecutive difference, and array conversions in the ArrayFunctions class

--- Main.java
+++ Main.java
@@ -5,7 +5,12 @@ class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in); 
         ArrayInput Ai = new ArrayInput(scan); // Create ArrayInput object
+        ArrayFunctions Af = new ArrayFunctions(); // Create ArrayFunctions object
 
         int[] userArray = Ai.userInput(); // Get array input from user
 
+        Af.oddEven(userArray); // Perform odd/even separation
+        Af.minDif(userArray); // Find minimum consecutive difference
+        Af.displayConversions(userArray); // Demonstrate array conversions
+    }
 }
