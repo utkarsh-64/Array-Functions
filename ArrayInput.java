ab77b11ac3ed78473a5caa65abd3ebf077ec3dba
Utkarsh <164793896+utkarsh-64@users.noreply.github.com>
2025-02-03 21:40:35 +0530

    Creating userInput method
    Create a userInput method to take input of the array from the user

--- ArrayInput.java
+++ ArrayInput.java
@@ -7,4 +7,19 @@ class ArrayInput {
     public ArrayInput(Scanner scan) {
         this.scan = scan;
     }
+
+    // Get integer array input from user
+    public int[] userInput() {
+        System.out.println("Enter the size of the array:");
+        int size = scan.nextInt(); // Read array size
+
+        int[] array = new int[size]; // Create array based on size
+
+        System.out.println("Enter " + size + " integers:");
+        for (int i = 0; i < size; i++) {
+            array[i] = scan.nextInt(); // Read each array element
+        }
+
+        return array;
+    }
 }
